52ee0b8391aec3843890f37af844310b231202be
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-01 03:28:43 +0600

    added token & token expiry & time 1.11min

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
+import { LOGIN_URL } from "../../../lib/spotify"
 
 export default NextAuth({
     // configure one or more providers here
@@ -10,5 +11,30 @@ export default NextAuth({
             authorization: LOGIN_URL,
         }),
     ],
-    // ... add more providers here
+    secret: process.env.JWT_SECRET,
+    pages: {
+        signIn: '/login'
+    },
+    callbacks: {
+        async jwt({ token, account, user }) {
+            // initial the user's session
+            if (account && user) {
+                return {
+                    ...token,
+                    accessToken: account.access_token,
+                    refreshToken: account.refresh_token,
+                    username: account.providerAccountId,
+                    accessTokenExpires: account.expires_at * 1000, // were handling expiry time in milliseconds hence the *1000
+                }
+            }
+
+            // return the previous token if the token is still valid
+            if (Date.now() < token.accessTokenExpires) {
+                console.log("token still valid");
+                return token
+            }
+            // refresh the user's session
+
+        }
+    }
 })
