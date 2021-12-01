2f6f7c1e7df2beb632cd265a590f02ac80bea406
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-12-02 03:42:17 +0600

    issue at usespotify.js line 5

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,13 +4,26 @@ import {
   LibraryIcon,
   PlusCircleIcon,
   RssIcon,
-  SearchIcon,
+  SearchIcon
 } from "@heroicons/react/outline";
+import { signOut, useSession } from "next-auth/react";
+import { useState } from 'react';
+import useSpotify from './../Hooks/useSpotify';
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+  console.log(session)
+
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 ">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide ">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log out</p>
+        </button>
+
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-6 w-6" />
           <p>Home</p>
